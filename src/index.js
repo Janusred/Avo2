31b4a42c841f281370c09e79ffd1f33735939b64
Janusred <horaciok300@hotmail.com>
2022-01-26 02:23:16 -0600

    Creacion de nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,32 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+console.log('Hola Janus :)')
+
+const url = "https://platzi-avo.vercel.app/api/avo";
+
+window.fetch(url).then(respuesta => respuesta.json())
+
+    .then((respuestaJson) => {
+        const todoslositem=[];
+
+        respuestaJson.data.forEach((item) => {
+            //imagen
+            const imagen = document.createElement("img");
+            //document.body.appendChild(imagen);
+            //Titulo
+            const titulo = document.createElement("h2");
+            //document.body.appendChild(titulo);
+            //precio
+            const price = document.createElement("div");
+            //document.body.appendChild(price);
+            
+            const container=document.createElement("div");
+            container.append(imagen,titulo,price);
+
+            todoslositem.push(container);
+        });
+document.body.append(...todoslositem);
+
+    });
+ 
