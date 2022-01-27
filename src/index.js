c28b941ab49e7090756efe31db436fa5d98c80c7
Janusred <horaciok300@hotmail.com>
2022-01-27 00:52:37 -0600

    Enriqueciendo informacion, Acomodando imagen y datos de api

--- src/index.js
+++ src/index.js
@@ -5,30 +5,37 @@
 
 console.log('Hola Janus :)')
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+//const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+const appNode=document.querySelector('#app');
 
-window.fetch(url).then(respuesta => respuesta.json())
+window.fetch(`${baseUrl}/api/avo`)
+    .then((respuesta) => respuesta.json())
 
     .then((respuestaJson) => {
-        const todoslositem=[];
+        const todoslositem = [];
 
         respuestaJson.data.forEach((item) => {
             //imagen
             const imagen = document.createElement("img");
+            imagen.src  = `${baseUrl}${item.image}`;
             //document.body.appendChild(imagen);
             //Titulo
             const titulo = document.createElement("h2");
+            titulo.textContent = item.name;
+            titulo.style= "font-size:2rem";
             //document.body.appendChild(titulo);
             //precio
             const price = document.createElement("div");
+            price.textContent = item.price;
             //document.body.appendChild(price);
-            
-            const container=document.createElement("div");
-            container.append(imagen,titulo,price);
+
+            const container = document.createElement("div");
+            container.append(imagen, titulo, price);
 
             todoslositem.push(container);
         });
-document.body.append(...todoslositem);
+        appNode.append(...todoslositem);
 
     });
- 
+
