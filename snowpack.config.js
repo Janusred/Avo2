6c4a6fd06f7bb4886be928e9b41a97cb2a2e96d6
Janusred <56011735+Janusred@users.noreply.github.com>
2022-01-28 23:45:05 -0600

    Update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,8 +1,8 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: '/',
-    src: '/',
+    public: '/index.html',
+    src: '/index.js',
   },
   buildOptions: {
     baseUrl: 'https://github.com/Janusred/Avo2',
